c78544be378c012debfadae31cf67005a41bb72c
fakhrul62 <ifakhrul23@gmail.com>
2025-06-23 03:45:30 +0600

    added new cors update

--- index.js
+++ index.js
@@ -28,23 +28,29 @@ app.use(cors({
 app.use((req, res, next) => {
   const allowedOrigins = [
     "http://localhost:5173",
+    "https://travel-agency-eight-kappa.web.app", // Firebase
     "https://travel-agency-eight-kappa.vercel.app",
     "https://travel-agency-nwn846xfo-fakhrul-alams-projects.vercel.app",
     "https://travel-agency-git-main-fakhrul-alams-projects.vercel.app",
-    "https://travel-agency-server-delta.vercel.app",
     "https://travel-agency-bu3n0al34-fakhrul-alams-projects.vercel.app",
-    "https://travel-agency-server-3n1wr27f2-fakhrul-alams-projects.vercel.app",
-    "https://travel-agency-server-3n1wr27f2-fakhrul-alams-projects.vercel.app/trips"
+    "https://travel-agency-server-delta.vercel.app",
+    "https://travel-agency-server-3n1wr27f2-fakhrul-alams-projects.vercel.app"
   ];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
   }
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204); // Preflight accepted
+  }
+
   next();
 });
+
 
 app.use(express.json());
 app.use(cookieParser());
